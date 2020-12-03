c7ae206a7baede393d7240d92eba604b22b23d25
Tedfeng0127 <60753095+Tedfeng0127@users.noreply.github.com>
2020-12-03 17:41:28 +0800

    Update get_customers.py

--- get_customers.py
+++ get_customers.py
@@ -1,15 +1,14 @@
-import requests             # 發出請求的模組
-import json                 # 將python內部的資料型態dict轉換成json檔案，或者反之
+import requests
+import json               # 將python內部的資料型態dict轉換成json檔案，或者反之
 
 # 原理:
 # 我們(client)發出request給華南銀行的伺服器(server)，
-# 華南銀行的server會辨別我們的request(也就是那一串url, 網址)，透過我們給予的參數(也就是api_key、uiid等)
+# 華南銀行的server會辨別我們的request(也就是那一串url, 網址)，透過我們給的參數(也就是api_key、uiid等)
 # 辨識我們要什麼回應(response)，回應就是回傳給我們的資料(有各種不同形式，有時候是網頁，有時候是pdf)
-# 而這個case的response是json檔案。(json檔案是網路傳輸資料的一種標準資料格式，資料規則簡單，需要的容量少，因此常常用以傳遞大量資料)
+# 在這個case裡面的response是json檔。(json檔是網路傳輸資料的一種標準資料格式，很輕量所以常常用以傳遞大量資料)
 
 
 # 發出request，將華南銀行server的response存在response這個變數(這一步就是透過API得到資料)
-# get是requests這個類別(class)裡面的一個函式，這個例子我們輸入兩個參數，一個是"url(地址)", 另一個是"headers"
 response = requests.get(url="https://www.fintechersapi.com/bank/huanan/getUUIDs?api_key=cef5e50c-c6df-46c0-86a8-69fa2fdc0fe1", headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"})
 
 # response印出來大概長這樣
@@ -30,8 +29,8 @@ response = requests.get(url="https://www.fintechersapi.com/bank/huanan/getUUIDs?
 
                                 
 # uuid_list 取得, 一次10筆
-# 用response.text把response轉成text(這樣才可以在python裡面處理)(是固定用法，先記著!)
-# 再用json這個類別裡面的函式loads把response.text的內容轉換成dict資料型態
+# 用response.text把response轉成text(這樣才能在python裡面做處理)
+# 再用json.loads把response.text的內容轉換成dict資料型態
 response = json.loads(response.text)        
 # print(response)            # 印出dict    
 # print(type(response))      # dict的資料型態
@@ -53,11 +52,11 @@ response = json.loads(response.text)
 #   "uuid": "833ecc53b7064974a0abaeb986da9c7a"
 # }
 while True:
-    i = 1         # 只是用來記數
+    i = 1         # 用來記數
     for uuid in response["uuid_list"]:
         # 取得保險資料
         resp_insurance = requests.get(url="https://www.fintechersapi.com/bank/huanan/insurance/record?uuid={}&api_key=cef5e50c-c6df-46c0-86a8-69fa2fdc0fe1".format(uuid), headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"})
-        # 下面這行是處理編碼問題，先記起來，有需要再弄懂!
+        # 處理編碼問題，先記起來!
         resp_insurance = resp_insurance.text.encode('ISO-8859-1').decode('utf-8')
         
         # 取得客戶基本資訊
@@ -67,7 +66,7 @@ while True:
         print("第{}筆資料, ".format(i))   # 標註是第幾筆資料
         i += 1
         print(f'uuid: {uuid}')           # 印出uuid來看一下
-        # 這三行都是把json資料轉成python的dict
+        # 這三行都是把json檔案的資料轉成python的dict
         resp_insurance = json.loads(resp_insurance)
         resp_client = json.loads(resp_client.text)
         resp_recent_10 = json.loads(resp_recent_10.text)
